2cb90c5aaa527254a10c39b0a5ed34086d4ba9c2
Shoumik Ahmed <141579922+shoumik011@users.noreply.github.com>
2024-07-28 14:05:37 +0600

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,8 +2,8 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList() {
-    const [showCart, setShowCart] = useState(false); 
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const dispatch = useDispatch();
+     const cartItems = useSelector(state => state.cart.items);
 
     const plantsArray = [
         {
@@ -245,7 +245,10 @@ const handlePlantsClick = (e) => {
    const handleContinueShopping = (e) => {
     e.preventDefault();
     setShowCart(false);
-  };
+    };
+    const handleAddToCart = (plant) => {
+        dispatch(addItem(plant));
+      };
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -268,7 +271,22 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+            {plantsArray.map((category, index) => (
+        <div key={index}>
+          <h1>{category.category}</h1>
+          <div className="product-list">
+            {category.plants.map((plant, plantIndex) => (
+              <div className="product-card" key={plantIndex}>
+                <img className="product-image" src={plant.image} alt={plant.name} />
+                <div className="product-title">{plant.name}</div>
+                <div className="product-description">{plant.description}</div>
+                <div className="product-price">${plant.cost}</div>
+                <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
 
         </div>
  ) :  (
